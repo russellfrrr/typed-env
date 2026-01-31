037fa53a9e68d799ae2d8257f95e7a8715ab4134
Russell Ferrero <russellferrero00@gmail.com>
2026-02-01 05:04:30 +0800

    feat: add schema based env parsing with node support

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,12 @@ export type Parser<T> = {
   parse(value: string | undefined): T;
 }
 
+type Schema = Record<string, Parser<any>>;
+
+type InferEnv<T extends Schema> = {
+  [K in keyof T]: T[K] extends Parser<infer R> ? R : never;
+}
+
 export const string = (): Parser<string> => {
   return {
     parse(value: string | undefined) {
@@ -30,4 +36,17 @@ export const number = (): Parser<number> => {
       return parsed;
     }
   }
+}
+
+export function createEnv<T extends Schema>(schema: T): InferEnv<T> {
+  const result = {} as InferEnv<T>;
+
+  for (const key in schema) {
+    const parser = schema[key];
+    const rawValue = process.env[key];
+
+    result[key] = parser.parse(rawValue);
+  }
+
+  return result;
 }
